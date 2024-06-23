7a0e0c57d1c115d0c70b15666184dc41f6dda14d
emirryilmazz <emiryilmaz.dev@gmail.com>
2024-06-23 22:18:58 +0300

    New rooms

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,18 +1,27 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stage } from "@react-three/drei";
 import Woman from "./Woman";
 import { Room } from "./Room";
+import Nightclub from "./Nightclub";
+import ConcertStage from "./ConcertStage";
 import { CameraControls } from "../controllers/CameraControls";
 import { LightControls } from "../controllers/LightControls";
 import { useHudSettings } from "../contexts/HudSettings";
+import { useCharacterAnimations } from "../contexts/CharacterAnimations";
 
 const Experience = () => {
   const { lightColor, lightIntensity, lightDistance, lightAngle } = useHudSettings();
+  const { animationIndex, setAnimationIndex, animations, setAnimations} = useCharacterAnimations()
+  console.log('animations', animationIndex)
   return (
     <>
       <CameraControls />
       <LightControls />
       <group position={[0, -1, 0]}>
         <Woman />
+        { ![1,3].includes(animationIndex) && <Room /> }
+        { animationIndex == 1 && <ConcertStage scale={[0.3,0.3,0.3]} position={[0,-1,0]} /> }
+        { animationIndex == 3 && <Nightclub /> }
+        
       </group>
       <mesh
         rotation={[-0.5 * Math.PI, 0, 0]}
