cb6f5a903245f1ce08c49a687baeb5e809f42505
saltukozelgul <saltukozelgul@gmail.com>
2024-06-24 18:16:17 +0300

    feat: enviorment musics added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { OrbitControls, Stage } from "@react-three/drei";
 import Woman from "./Woman";
 import Nightclub from "./Nightclub";
@@ -11,10 +12,47 @@ import { useCharacterCustomization } from "../contexts/CharacterCustomization";
 import Man from "./Man";
 import NeonBedroom from "./NeonBedroom";
 
+import concertStageMusic from "../assets/guitar.mp3";
+import parkMusic from "../assets/park.mp3";
+import nightclubMusic from "../assets/nightclub.mp3";
+
 const Experience = () => {
   const { lightColor, lightIntensity, lightDistance, lightAngle } = useHudSettings();
   const { animationIndex, setAnimationIndex, animations, setAnimation} = useCharacterAnimations()
   const { character, scene } = useCharacterCustomization()
+  
+  const [audio, setAudio] = useState(null);
+
+  useEffect(() => {
+    if (audio) {
+      audio.pause();
+    }
+
+    let newAudio;
+    switch (scene) {
+      case 0:
+        //newAudio = new Audio(neonBedroomMusic);
+        break;
+      case 1:
+        newAudio = new Audio(concertStageMusic);
+        break;
+      case 2:
+        newAudio = new Audio(parkMusic);
+        break;
+      case 3:
+        newAudio = new Audio(nightclubMusic);
+        break;
+      default:
+        break;
+    }
+
+    if (newAudio) {
+      newAudio.loop = true;
+      newAudio.play();
+      setAudio(newAudio);
+    }
+  }, [scene]);
+
   return (
     <>
       <CameraControls />
